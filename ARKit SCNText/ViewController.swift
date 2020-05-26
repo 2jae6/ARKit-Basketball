1cfce6e961d363aff98c60d6acd0b78a27ba6fd7
JaeWook Lee <2jae66@gmail.com>
2020-05-26 21:24:20 +0900

    back up

---
+++ ARKit SCNText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKit SCNText
+//
+//  Created by 1 on 2020/05/26.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
